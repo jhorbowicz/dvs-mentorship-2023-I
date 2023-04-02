5ffcede089044c4000e6aada76b677a2e2cef35a
Jan Horbowicz <janhorbowicz@gmail.com>
2023-04-02 12:56:58 +0200

    🔧 Apply suggestions
    - use 0 and d3.max() to define yDomain
    - absolutie positioning for the legend
    - use accessors instead of object properties
    - clarify the naming (use width and hight
      instead of xMax and yMax)

--- src/week-one/ChartOne.jsx
+++ src/week-one/ChartOne.jsx
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { scaleLinear, extent, schemeCategory10, scaleOrdinal } from "d3"
+import { scaleLinear, extent, max, schemeCategory10, scaleOrdinal } from "d3"
 import { AxisLeft, AxisBottom } from "@visx/axis"
 import { Grid } from "@visx/grid"
 import { LegendOrdinal } from "@visx/legend"
@@ -7,49 +7,46 @@ import { LegendOrdinal } from "@visx/legend"
 import ScatterplotOne from "./ScatterplotOne";
 
 export default function ChartOne({dataset, width, height}) {
-  const weightAccessor = (d) => d["Weight_in_lbs"]
-  const hpAccessor = (d) => d["Horsepower"]
-  const mpgAccessor = (d) => d["Miles_per_Gallon"]
+  const xAccessor = (d) => d["Horsepower"]
+  const yAccessor = (d) => d["Miles_per_Gallon"]
   const originAccessor = (d) => d["Origin"]
-  const xMax = width
-  const yMax = height
 
    const yScale = useMemo(() => {
     return scaleLinear()
-      .domain(extent(dataset, mpgAccessor))
-      .range([yMax, 0])
+      .domain([0, max(dataset, yAccessor) + 5])
+      .range([height, 0])
   })
 
   const xScale = useMemo(() => {
     return scaleLinear()
-      .domain(extent(dataset, hpAccessor))
-      .range([0, xMax])
+      .domain(extent(dataset, xAccessor))
+      .range([0, width])
   })
 
   const colorScale = useMemo(() => {
     const uniqueOrigins = [...new Set(dataset.map(originAccessor))]
     return scaleOrdinal().domain(uniqueOrigins).range(schemeCategory10)
   })
- 
+
+  const hasBothXandY = (d) => xAccessor(d) && yAccessor(d)
+
   return (
-    <svg width={width} height={height + 50} className="m-10">
-      <g transform="translate(50, 0)">
-        <AxisLeft scale={yScale} />
-      </g>
-      <g transform="translate(50, 0)">
-        <Grid xScale={xScale} yScale={yScale} width={xMax - 50} height={yMax} />
-      </g>
-      <g transform="translate(50, 0)">
-        <ScatterplotOne data={dataset} xScale={xScale} yScale={yScale} xKey="Horsepower" yKey="Miles_per_Gallon" colorAccessor={originAccessor} colorScale={ colorScale} />
-      </g>
-      <g transform={`translate(50, ${height})`}>
-        <AxisBottom scale={xScale} />
-      </g>
-      <g transform={`translate(600, 0)`}>
-        <LegendOrdinal 
-          scale={colorScale}
-        />
-      </g>
-    </svg>
+    <div className="relative" style={{maxWidth: width}}>
+      <svg viewBox={`0 0 ${width + 100} ${height}`} width={width} height={height} className="m-10">
+        <g transform="translate(50, 0)">
+          <AxisLeft scale={yScale} />
+        </g>
+        <g transform="translate(50, 0)">
+          <Grid xScale={xScale} yScale={yScale} width={width - 50} height={height} />
+        </g>
+        <g transform="translate(50, 0)">
+          <ScatterplotOne data={dataset.filter(hasBothXandY)} xScale={xScale} yScale={yScale} xAccessor={xAccessor} yAccessor={yAccessor} colorAccessor={originAccessor} colorScale={colorScale} />
+        </g>
+        <g transform={`translate(50, ${height})`}>
+          <AxisBottom scale={xScale} />
+        </g>
+      </svg>
+      <LegendOrdinal scale={colorScale} className="absolute top-10 right-4"/>
+    </div>
   )
 }
--- src/week-one/ScatterplotOne.jsx
+++ src/week-one/ScatterplotOne.jsx
@@ -1,18 +1,18 @@
-export default function ScatterplotOne({ data, xScale, yScale, xKey, yKey, colorAccessor, colorScale }) {
-  
+export default function ScatterplotOne({ data, xScale, yScale, xAccessor, yAccessor, colorAccessor, colorScale }) {
+
   return (
     <>
       {data.map((dataEntry, i) => {
-        return dataEntry[xKey] && dataEntry[yKey] ? ( // TODO move "flaky data" filtering to a higher level
+        return (
           <circle
             key={`dataEntry.name ${i}`}
             r="3"
-            cx={xScale(dataEntry[xKey])}
-            cy={yScale(dataEntry[yKey])}
+            cx={xScale(xAccessor(dataEntry))}
+            cy={yScale(yAccessor(dataEntry))}
             opacity={1}
             fill={colorScale(colorAccessor(dataEntry))}
             fillOpacity={1}
-          />) : null
+          />)
       })}
     </>
   )
